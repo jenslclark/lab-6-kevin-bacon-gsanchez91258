4c04049caf877918c237b4007676821e4d04cdd3
gsanchez91258 <gsanchez@lclark.edu>
2017-03-20 15:05:41 -0700

    Update common.php

--- common.php
+++ common.php
@@ -1,4 +1,23 @@
 <?php
 
+/*** connection credentials *******/
+$servername = "www.watzekdi.net";
+$username = "watzekdi_cs393";
+$password = "KevinBac0n";
+$database = "watzekdi_imdb";
+$dbport = 3306;
+
+
+/****** connect to database **************/
+
+try {
+$db = new PDO("mysql:host=$servername;dbname=$database;charset=utf8;port=$dbport", $username, $password);
+}
+catch(PDOException $e) {
+echo $e->getMessage();
+}
+
+
+
 
 ?>
